93c18a03986d64ae021eccc22a17655ab2cde9ce
Vasil Radnev <160313405+vradnev19@users.noreply.github.com>
2025-09-18 10:54:35 +0300

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
